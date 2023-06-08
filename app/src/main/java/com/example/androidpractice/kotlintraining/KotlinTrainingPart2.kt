38f6b5a9ef80d9023c7d58b322846f6805995944
Rodion Gazizov <rodion.gazizov@bk.ru>
2023-06-08 09:20:48 +0300

    create user data class

--- app/src/main/java/com/example/androidpractice/kotlintraining/KotlinTrainingPart2.kt
+++ app/src/main/java/com/example/androidpractice/kotlintraining/KotlinTrainingPart2.kt
@@ -1,9 +1,29 @@
 package com.example.androidpractice.kotlintraining
 
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+
 /*
     Создать enum Type с константами DEMO и FULL.
  */
 enum class Type {
     DEMO,
     FULL
+}
+
+
+/*
+    Реализовать класс данных User с полями id, name, age и type.
+    У класса User создать ленивое свойство startTime, в котором получаем текущее время.
+ */
+data class User(
+    val id: String,
+    val name: String,
+    val age: Int,
+    val type: Type
+) {
+    val startType by lazy {
+        Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+    }
 }
