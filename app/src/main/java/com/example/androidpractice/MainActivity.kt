5e3c7cbb6d61b2bec9503222427f782501f32c8d
Rodion Gazizov <rodion.gazizov@bk.ru>
2023-06-14 10:18:48 +0300

    add bottom navigation view

--- app/src/main/java/com/example/androidpractice/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice/MainActivity.kt
@@ -2,16 +2,58 @@ package com.example.androidpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
+import com.example.androidpractice.databinding.ActivityMainBinding
 import com.example.androidpractice.screen.profile.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
+        setupNavigation()
+        navigate(R.id.profileNavItem)
+    }
+
+    private fun navigate(navItemId: Int) {
+        binding.bottomNavView.selectedItemId = navItemId
+    }
+
+    private fun navigate(fragment: Class<out Fragment>) {
         supportFragmentManager.commit {
-            this.replace(R.id.container, ProfileFragment::class.java, null)
+            replace(R.id.container, fragment, null)
+        }
+    }
+
+    private fun setupNavigation() {
+        binding.bottomNavView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.newsNavItem -> {
+                    true
+                }
+                R.id.searchNavItem -> {
+                    true
+                }
+                R.id.historyNavItem -> {
+                    true
+                }
+                R.id.profileNavItem -> {
+                    navigate(ProfileFragment::class.java)
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+        binding.helpButton.setOnClickListener {
+            binding.bottomNavView.selectedItemId = R.id.helpNavItem
         }
     }
 }
